3e31bff5075420168c93902a5c9eaa34b63f4f2d
Konstantin <konstantinstepanyan99@gmail.com>
2021-10-12 00:10:38 +0300

    done only list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
+//import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
